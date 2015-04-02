5abdc63de0271afe3c1ddcf035eee9ea0b9b16ec
Owen <owen.dismuke@gmail.com>
2015-04-02 11:56:20 -0700

    Added returnable function.
    Now there is a returnable version in addition to the console logging version.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,4 @@
-function FizzBuzz(fizzNum, buzzNum, length){
+function FizzBuzzConsole(fizzNum, buzzNum, length){
   for(var i = 0; i < (length || 100);){
     var out = '';
     i++;
@@ -13,4 +13,22 @@ function FizzBuzz(fizzNum, buzzNum, length){
     
     console.log(out || i);
   }
+};
+
+function FizzBuzzNum(checkNum, fizzNum, buzzNum){
+  var out = '';
+  
+  if(!Number.isInteger(checkNum)) {
+    return 'Check number is not a valid integer!';
+  }; 
+  
+  if (checkNum % (Number.isInteger(fizzNum) ? fizzNum : 3) === 0){
+  	 out += 'Fizz';
+  };
+  
+  if (checkNum % (Number.isInteger(buzzNum) ? buzzNum : 5) === 0){
+  	out += 'Buzz';
+  };
+  
+  return out || checkNum;
 };
